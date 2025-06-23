d51d220f36bdbc6b7ef7e1529622723bf6b155b5
Abhishek <github.abhiarya@gmail.com>
2025-06-23 13:02:21 +0530

    init-turbo from start

---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from "@abhiarya/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="container mx-auto bg-red-400 scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-      <section className="mx-auto w-full max-w-2xl bg-blue-400">asdf</section>
-    </main>
-  );
-}
