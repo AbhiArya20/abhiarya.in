8f24c95f5f362b39a0bd637fe27280ab02a59059
Abhishek <github.abhiarya@gmail.com>
2025-06-23 15:44:09 +0530

    added: shadcn

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google"
+import "@abhiarya/ui/globals.css";
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export const metadata: Metadata = {
+  title: "Abhi Arya",
+  description: "Portfolio of Abhi Arya",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@abhiarya/ui/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Button>Hello</Button>
+    </div>
+  );
+}
